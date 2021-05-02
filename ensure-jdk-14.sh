5770d8e36e36e0ad0071d5d14701b7a139afc960
Hayden Taylor <hayden@avuna.net>
2021-05-02 02:22:40 -0400

    Updated Jitpack to JDK 14 really REALLY this time

---
+++ ensure-jdk-14.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+JV=`java -version 2>&1 >/dev/null | head -1`
+echo $JV | sed -E 's/^.*version "([^".]*)\.[^"]*".*$/\1/'
+
+if [ "$JV" != 14 ]; then
+	case "$1" in
+	install)
+		echo "installing sdkman..."
+		curl -s "https://get.sdkman.io" | bash
+		source ~/.sdkman/bin/sdkman-init.sh
+		sdk install java 14.0.2-adpt
+		;;
+	use)
+		echo "must source ~/.sdkman/bin/sdkman-init.sh"
+		exit 1
+		;;
+	esac
+fi
